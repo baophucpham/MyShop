d8de9bb05b505263810e7a3e8d8c738f09f5f2f4
baophucpham <phucpham@meu-solutions.com>
2020-08-19 17:40:07 +0700

    home screen

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React,{Component} from 'react';
 import {View,Text} from 'react-native';
+import { NavigationContainer} from "react-navigation/native";
+import{createStackNavigator } from "react-navigation/stack"
+import Home from './src/components/Home'
 
   
 export default class App extends Component{
   render(){
     return(
-      <View>
-        <Text>day la component App</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" Component={Home}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     )
   }
 };
